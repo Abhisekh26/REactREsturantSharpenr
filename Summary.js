8a4fc8db54d9a391ad1f2657faf7cbcb2938a918
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2024-01-29 00:02:01 +0530

    Add files via upload

---
+++ Summary.js
@@ -0,0 +1,16 @@
+import "./Div.css";
+function Summary() {
+  return (
+    <section className="summary-display">
+      <h1>Delecious Food, Delivered To You</h1>
+      <p>
+        {" "}
+        Choose your favorite meal from our board selection of available meals
+        and enjoy a delecious lunch or dish at home All our meals are cooked
+        with high quality ingedritents just in time and of course by experienced
+        chefs
+      </p>
+    </section>
+  );
+}
+export default Summary;
